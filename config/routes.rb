240289f2b368041823c9c039f3b362202e66d304
Uwais-progton <uwais@progton.com>
2024-02-06 16:34:33 +0530

    added styles

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resource :password
   root "main#index"
   resources :products
+  delete '/products/:id', to: 'products#destroy', as: 'delete_product'
   resource :cart, only: [:show]
   post 'add_to_cart/:product_id', to: 'carts#add_to_cart', as: 'cart_add_to_cart'
   delete '/remove_from_cart/:id', to: 'carts#remove_from_cart', as: 'remove_from_cart'
