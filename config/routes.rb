9cca437e61bb7ca7670b116c93871e2dc1a4b157
Uwais-progton <uwais@progton.com>
2024-02-06 11:55:04 +0530

    added CREATE operation to products for admin

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   resources :products
   resource :cart, only: [:show]
   post 'add_to_cart/:product_id', to: 'carts#add_to_cart', as: 'cart_add_to_cart'
-  delete 'remove_from_cart/:id', to: 'carts#remove_from_cart', as: 'remove_from_cart'
+  delete '/remove_from_cart/:id', to: 'carts#remove_from_cart', as: 'remove_from_cart'
 
 end
